f99966005bdac7174dfe0b5954fac05026c61b18
Ripperoni <email@ripperoni.com>
2026-02-07 15:40:54 -0800

    Fixed lint issues in test

---
+++ test/scripts/deno-imports.d.ts
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+declare module 'jsr:*' {
+  const value: any
+  export default value
+  export const Database: any
+  export type Database = any
+  export const load: any
+}
+declare module 'npm:*' {
+  const value: any
+  export default value
+  export const chromium: any
+  export type Page = any
+  export type Browser = any
+}
