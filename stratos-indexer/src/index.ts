4020a1be98f48014c91a5884f7ff331c1e780266
Evelyn Osman <ashex@chipnick.com>
2026-03-14 11:14:54 +0100

    Fancy new indexer, hope it works

---
+++ stratos-indexer/src/index.ts
@@ -0,0 +1,20 @@
+export { Indexer } from './indexer.ts'
+export { loadConfig } from './config.ts'
+export type { IndexerConfig } from './config.ts'
+export { WorkerPool } from './worker-pool.ts'
+export { CursorManager } from './cursor-manager.ts'
+export { PdsFirehose, processFirehoseWork } from './pds-firehose.ts'
+export type { EnrollmentCallback } from './pds-firehose.ts'
+export {
+  StratosServiceSubscription,
+  StratosActorSync,
+  indexStratosRecord,
+  deleteStratosRecord,
+} from './stratos-sync.ts'
+export { backfillRepos } from './backfill.ts'
+export {
+  decodeCommitOps,
+  parseCid,
+  jsonToLex,
+  extractBoundaries,
+} from './record-decoder.ts'
