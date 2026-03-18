b6a83899eed04e119689aa3c249e564049c84518
Evelyn Osman <ashex@chipnick.com>
2026-03-18 19:09:20 +0100

    offload MST computation to worker thread pool

--- stratos-service/src/features/mst/index.ts
+++ stratos-service/src/features/mst/index.ts
@@ -1,2 +1,3 @@
 export { StratosBlockStoreReader } from './storage-adapter.js'
 export { signAndPersistCommit, type SignedCommitResult } from './signer.js'
+export { CommitPool } from './commit-pool.js'
